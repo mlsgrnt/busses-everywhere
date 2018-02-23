9c16e48a2f9f361d6a85f23b8845d7d784b86eac
Blydro Klonk <blydro@gmail.com>
2018-02-23 16:55:05 +0100

    good code

--- src/Locate.js
+++ src/Locate.js
@@ -35,10 +35,12 @@ class Locate extends Component {
 		navigator.geolocation.watchPosition(
 			position => {
 				if (
-					position.coords.latitude - this.state.position.coords.latitude >
-						0.00001 ||
-					position.coords.longitude - this.state.position.coords.longitude >
-						0.00001
+					Math.abs(
+						position.coords.latitude - this.state.position.coords.latitude
+					) > 0.00001 ||
+					Math.abs(
+						position.coords.longitude - this.state.position.coords.longitude
+					) > 0.00001
 				) {
 					this.setState({
 						position
