227d0ee66a06e4243dcbd1c38b92ccd36a55b33c
Blydro Klonk <blydro@gmail.com>
2018-02-25 14:33:54 +0100

    arrivals are prettier

--- src/Arrivals.js
+++ src/Arrivals.js
@@ -19,9 +19,20 @@ class Arrivals extends Component {
 		};
 
 		const renderDirection = direction => {
-			if (direction.split(', ')[1] !== undefined) {
-				//if the destiantion has weird commas in it!
+			if (
+				direction.split(', ')[1] !== undefined &&
+				direction.split(', ')[1].length > 4
+			) {
+				//if the destiantion has weird commas in it! but we don't want to wind up with Hbf either
 				direction = direction.split(', ')[1];
+			}
+
+			//strip S + U
+			if (direction.split('S ')[1] !== undefined) {
+				direction = direction.split('S ')[1];
+			}
+			if (direction.split('U ')[1] !== undefined) {
+				direction = direction.split('U ')[1];
 			}
 
 			return direction;
